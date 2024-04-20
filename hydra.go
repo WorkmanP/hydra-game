1214bdc2d8304d4c52f46a8160b578dff726e1c4
Paul Workman <75460703+WorkmanP@users.noreply.github.com>
2024-04-20 13:57:08 +0100

    Add clarifying comments

--- hydra.go
+++ hydra.go
@@ -7,6 +7,7 @@ import (
 type NegativeHeadsError struct{}
 
 func (*NegativeHeadsError) Error() string {
+	// Guards against integer underflow
 	return "Cannot have negative heads"
 }
 func main() {
@@ -40,14 +41,13 @@ func processTree (heads, stubs []int) (int) {
 
 	// If the count is the same, nothing has changed, so the heads list is
 	// empty...
-
-	// This is just a while loop as go doesn't have a separate keyword
 	for count != tempCount {
+		// This is just a while loop, as Go doesn't have a separate keyword
 		count = tempCount
 		heads, stubs, tempCount = removeLowest(heads, stubs, count)
 	}
 
-	// As it return the current step, the number required to kill the hydra
+	// As it finds the current step, the number required to kill the hydra
 	// is one less...
 	return count-1
 }
@@ -57,26 +57,35 @@ func removeLowest (heads, stubs []int, count int) ([]int, []int, int) {
 			panic(&NegativeHeadsError{})
 		}
 		if i == 0 && val != 0 {
+			// Heads that don't generate others can be killed in
+			// one step. Killing them all at once reduces load times
 			count += heads[0]
 			heads[0] = 0
 			return heads, stubs, count
 		}
 
 		if val == 0 {
+			// No heads, move up the tree
 			continue
 		}
+		// We have found a head so kill it
 		heads[i]--
+
+		// Increase the number of heads below by the step count
 		heads[i-1] += count
+
+		// Increase the count
 		count++
 
+		// Check whether we have to convert the node to a head
 		headsAbove := 0
-
 		for j, val := range (heads) {
 			if j <= i {continue}
 			headsAbove += val
 		}
+		
 		if val == 1 && headsAbove == 0{
-			// Add the new stub create to the list of heads
+			// Add the new stub created to the list of heads
 			heads[i-1]++
 			stubs[i-1]=0
 		}
